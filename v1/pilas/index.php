b91b91023a3b9a735db181a03c68414590453e23
pablo <pablomaganapablomagub@gmail.com>
2015-12-11 19:57:28 +0100

    pilas impletant calculos

--- v1/pilas/index.php
+++ v1/pilas/index.php
@@ -1,34 +1,40 @@
 <?php
-        require_once("../data/contenedor.php");
+require_once("../data/contenedor.php");
+require_once('/../data/calculos.php');
 
-        $fuente = file_get_contents("http://mapas.valencia.es/lanzadera/opendata/RES_PILAS/JSON");
+$fuente = file_get_contents("http://mapas.valencia.es/lanzadera/opendata/RES_PILAS/JSON");
 
-        $fuente_decode=json_decode($fuente);
+$fuente_decode = json_decode($fuente);
 
-        $contenedores_pilas=obtenerPilas($fuente_decode);
+$contenedores_pilas = obtenerPilas($fuente_decode);
 
-        //print_r($contenedores_pilas);
+//print_r($contenedores_pilas);
 
-        $arrayPilas=array();
-        print_r(count($contenedores_pilas));
+$arrayPilas = array();
 
-        foreach($contenedores_pilas as $pila){
-            $id=$pila->properties->id;
-            $tipo="pilas";
-            $direccion=$pila->properties->direccion;
-            $centro=$pila->properties->centro;
-            $lat=$pila->geometry->coordinates[1];
-            $log=$pila->geometry->coordinates[0];
 
-            $contentPilas=new Contedor($id,$tipo,$direccion,$centro,$lat,$log);
-            $arrayPilas[] = $contentPilas;
-            array_push($arrayPilas,$contentPilas);
-        }
-        $newjson=json_encode($arrayPilas);
-        print $newjson;
-        //print_r($arrayPilas);
+foreach ($contenedores_pilas as $pila) {
+    $id = $pila->properties->id;
+    $tipo = "pilas";
+    $direccion = $pila->properties->direccion;
+    $centro = $pila->properties->centro;
+    $lat = $pila->geometry->coordinates[1];
+    $long = $pila->geometry->coordinates[0];
 
-    function obtenerPilas($contenedores_pilas){
-        $pilas=$contenedores_pilas->features;
-        return $pilas;
+    $contentPilas = new Contedor($id, $tipo, $direccion, $centro, $lat, $long);
+
+    if (($distance = Calculos::obtenerCalculos()->getDistance($lat, $long, 39.469763, -0.376387)) < 200) {
+        array_push($arrayPilas, $contentPilas);
     }
+
+}
+print_r(count($arrayPilas));
+$newjson = json_encode($arrayPilas);
+print $newjson;
+//print_r($arrayPilas);
+
+function obtenerPilas($contenedores_pilas)
+{
+    $pilas = $contenedores_pilas->features;
+    return $pilas;
+}
