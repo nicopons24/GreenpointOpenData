d104ea175fe85db750e0b3ab722df1b6f14f4c4b
nicopons24 <nicopons24@gmail.com>
2015-12-11 19:50:03 +0100

    Calcula la posicion cercana x

--- v1/aceite/index.php
+++ v1/aceite/index.php
@@ -1,2 +1,39 @@
 <?php
-$fuente = file_get_contents("http://mapas.valencia.es/lanzadera/opendata/res_aceite/JSON");
+require '/../data/contenedor.php';
+require '/../data/calculos.php';
+
+const URL_ACEITE = "http://mapas.valencia.es/lanzadera/opendata/res_aceite/JSON";
+
+$fuente = file_get_contents(URL_ACEITE);
+$body = json_decode($fuente);
+
+$features = obtenerArrayContendores($body);
+$listaContenedores = obtenerInformacionContenedores($features);
+print json_encode($listaContenedores);
+
+function obtenerInformacionContenedores($array) {
+    $contenedores = array();
+    foreach($array as $aceite) {
+        $tipo = "aceite";
+        $id = $aceite->properties->id;
+        $direccion = $aceite->properties->direccion;
+        $centro = $aceite->properties->centro;
+        $long = $aceite->geometry->coordinates[0];
+        $lat = $aceite->geometry->coordinates[1];
+        $c = new Contedor($id, $tipo, $direccion, $centro, $lat, $long);
+        if (($distance = Calculos::obtenerCalculos()->getDistance($lat, $long, 39.471210, -0.408766)) < 200) {
+            array_push($contenedores, $c);
+        }
+    }
+    return $contenedores;
+}
+
+function obtenerArrayContendores($body)
+{
+    $listaContenedores = $body->features;
+    return $listaContenedores;
+}
+
+function getDistance(  ) {
+
+}
---
+++ v1/data/calculos.php
@@ -0,0 +1,34 @@
+<?php
+
+class Calculos
+{
+    private static $instance = null;
+
+    final private function __construct()
+    {
+
+    }
+
+    public static function obtenerCalculos()
+    {
+        if (self::$instance == null) {
+            self::$instance = new Calculos();
+        }
+
+        return self::$instance;
+    }
+
+    public function getDistance($latContenedor, $longCont1, $latUser, $longUser)
+    {
+        $earth_radius = 6371;
+
+        $dLat = deg2rad($latUser - $latContenedor);
+        $dLon = deg2rad($longUser - $longCont1);
+
+        $a = sin($dLat / 2) * sin($dLat / 2) + cos(deg2rad($latContenedor)) * cos(deg2rad($latUser)) * sin($dLon / 2) * sin($dLon / 2);
+        $c = 2 * asin(sqrt($a));
+        $d = $earth_radius * $c;
+
+        return $d * 1000;
+    }
+}
