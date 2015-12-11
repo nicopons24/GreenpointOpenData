c91e871442e21bc10914c6813da0386ca48e8ea7
nicopons24 <nicopons24@gmail.com>
2015-12-11 15:40:18 +0100

    Initial commit

---
+++ v1/pilas/index.php
@@ -0,0 +1,13 @@
+<?php
+    //http://gobiernoabierto.valencia.es/wp-content/themes/viavansi-ogov/proxyFile.php?url=http://mapas.valencia.es/lanzadera/opendata/RES_PILAS/JSON
+    $fuente=file_get_contents("http://gobiernoabierto.valencia.es/wp-content/themes/viavansi-ogov/proxyFile.php?url=http://mapas.valencia.es/lanzadera/opendata/RES_PILAS/JSON");
+
+//print($fuente);
+
+
+    //$latitud=$_POST["latitud"];
+    //$longitud=$_POST["longitud"];
+
+    $resultadoFiltrado=json_encode(json_encode($fuente));
+    print($resultadoFiltrado);
+?>
